3bdb32330968f36a373aeb29de54aee4a34c254b
Conrad Koh <conradkoh@gmail.com>
2020-05-24 12:04:11 +0800

    Added test suites

---
+++ test/sample.js
@@ -0,0 +1,8 @@
+import { assert, expect } from 'chai';
+describe('Sample Suite', async () => {
+    beforeEach(async () => {});
+    afterEach(async () => {});
+    it('Sample', async () => {
+        assert.isTrue(true, 'Sample test ok.');
+    });
+});
