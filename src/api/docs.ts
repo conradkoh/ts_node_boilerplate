c0a44cbe2600e0bf5024cf25d2ca0ac15a982a4e
Conrad Koh <conradkoh@gmail.com>
2020-08-01 13:28:52 +0800

    Removed docs console log

--- src/api/docs.ts
+++ src/api/docs.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import path from 'path';
 const docsFilePath = path.resolve(path.join(__dirname, '../../doc'));
-console.log('loading docs from ' + docsFilePath);
 const router = express.Router();
 router.use('/', express.static(docsFilePath));
 export default router;
